942d04915ce6a8bc8e5b8768ef320ca70260d5a1
ming <mick66145@gmail.com>
2025-03-10 22:26:07 +0800

    chore: 更改名稱跟版本

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ var config = {};
 config['domain'] = 'www.iplusonego.com';
 config['restAPI'] = 'https://www.iplusonego.com/api/index.php';
 config['import_version'] = '1.2';
-config['title'] = '愛+1';
+config['title'] = '社群智慧整單平台賣家系統';
 config['version'] = 'PRODUCTION';
 config['chrome_ext'] = 'https://chrome.google.com/webstore/detail/iplusone/ojkkbclemoghbemdkcgdoinakkbfpkdk';
