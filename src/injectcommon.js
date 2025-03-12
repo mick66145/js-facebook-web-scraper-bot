d3ef6b2284a8a45706f019327bcbd5d93a38559d
ming <mick66145@gmail.com>
2025-03-12 18:17:41 +0800

    refactor

--- src/injectcommon.js
+++ src/injectcommon.js
@@ -1,2 +1,6 @@
-var d = document["getElementById"]("ipo_extension_id");
-window["IPO_extension_id"] = d["getAttribute"]("ipo_extension_id"), window["IPO_extension_version"] = d["getAttribute"]("ipo_extension_version"), d["parentNode"]["removeChild"](d);
+const extensionElement = document.getElementById('ipo_extension_id');
+
+window.IPO_extension_id = extensionElement.getAttribute('ipo_extension_id');
+window.IPO_extension_version = extensionElement.getAttribute('ipo_extension_version');
+
+extensionElement.parentNode.removeChild(extensionElement);
